baec30c07e434001d4ca9b5e44857004970f4f5e
Marroquin1 <luis.marroquin@catolica.edu.sv>
2024-04-05 23:48:20 -0600

    Agregar archivos de proyecto.

---
+++ Practica09Helper/Controllers/EquiposController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Practica09Helper.Controllers
+{
+    public class EquiposController : Controller
+    {
+        private readonly equiposDbContext _equiposDbContext;
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Practica09Helper/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Practica09Helper.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
